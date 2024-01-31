810eb6e70acf147700048a69474e8aa6595d2742
David <euntek95@gmail.com>
2024-01-31 14:40:36 -0800

    Finish phase 1

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+require_relative 'piece.rb'
+
 class Board
     def initialize
         @rows = Array.new(8) {Array.new(8)}
@@ -14,7 +16,19 @@ class Board
     end
 
     def move_piece(start_pos, end_pos)
-
+        if self[start_pos].nil?  || self[end_pos] != nil
+            raise 'No piece to move'
+        else
+            piece = self[start_pos] 
+            self[end_pos] = piece 
+            piece.pos = end_pos
+            self[start_pos] = nil
+        end
     end
-
+    #testing
 end
+
+b = Board.new
+p = Piece.new("black", b, [0,0])
+b[[0,0]] = p 
+b.move_piece([0,0],[1,1])
--- piece.rb
+++ piece.rb
@@ -1,6 +1,11 @@
-def Piece
-    def initialize
+class Piece
+    
+    attr_accessor :color, :board, :pos
+    def initialize(color, board, pos)
         # locations for start for Queen`/`Rook`/`Knight
         # nullpiece = nil location
+        @color = color
+        @board = board
+        @pos = pos
     end
 end 
